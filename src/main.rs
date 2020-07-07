1a330debff870a41782d61e90fdd2789e1f7751e
dmitri9149 <dmitri.as.name@gmail.com>
2020-07-07 11:08:28 +0300

    loop is added

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,15 @@ fn main() {
 
     println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    loop {
+        println!("Please input your guess.");
+
+        // --snip--
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
